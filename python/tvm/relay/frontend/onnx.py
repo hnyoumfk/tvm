e80204836c689936c7e508d76d02d4122c4f6c52
hnyoumfk <hnyoumfk@gmail.com>
2022-03-22 10:13:11 -0700

    add line for test

--- python/tvm/relay/frontend/onnx.py
+++ python/tvm/relay/frontend/onnx.py
@@ -3831,6 +3831,7 @@ class QAttention(OnnxOpConverter):
         #TODO: output_shape = (batch_size, seq_len, hidden_size)
 
         # TODO: 220 for ORT
+        # TODO: 220 for ORT
 
         # TODO: Dequant
 
